8b4f31f372b71addb223ce4c3ee2b95521d4dfc4
gammahazard <dhaliswag777@gmail.com>
2024-05-26 15:16:58 -0400

    test

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -20,8 +20,8 @@ const theme = createTheme({
         root: {
           color: 'white',
           borderColor: 'white',
-          borderRadius:0,
-
+          borderRadius:5,
+          backgroundColor: 'rgba(255, 255, 255, 0.5)',
           '&:hover': {
             backgroundColor: 'rgba(255, 255, 255, 0.1)',
             color: 'white'
