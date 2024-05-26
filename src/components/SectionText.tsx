d9cddc192ca0b1642f90892c5c0ba9bb5d6b0092
gammahazard <dhaliswag777@gmail.com>
2024-05-26 19:52:20 -0400

    test different style

--- src/components/SectionText.tsx
+++ src/components/SectionText.tsx
@@ -11,7 +11,7 @@ const SectionText = () => {
   return (
     <Box 
       display="flex" 
-      flexDirection="row" 
+      flexDirection={{ xs: 'column', md: 'row' }} 
       alignItems="center" 
       justifyContent="space-between" 
       fontFamily="pixelFont"
@@ -21,18 +21,16 @@ const SectionText = () => {
     >
       <Box flex={1} p={2}>
         <Typography 
-          variant="h4" 
+          variant="h2" 
           gutterBottom 
           align="left"
           style={{
             color: '#E1EBFA',
-            fontSize: '60px',
-            fontFamily: '"pixelFont"',
+            fontSize: '3rem',
             fontWeight: 700,
-            lineHeight: '70px',
           }}
         >
-          CyberVerse
+          CYBERVERSE
         </Typography>
         <Typography 
           variant="body1" 
@@ -40,72 +38,43 @@ const SectionText = () => {
           align="left"
           style={{
             color: '#E1EBFA',
-            fontSize: '22px',
-            width: '70%',
-            fontFamily: '"pixelFont"',
-            lineHeight: '26px',
+            fontSize: '1.2rem',
+            lineHeight: '1.5',
             marginBottom: '20px',
           }}
         >
-          Cyberverse is a vast open world where you can play, earn and have fun bmlamgleamglreamgrlamgarl
+          Cyberverse is a vast open world where you can play, earn and have fun. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia purus non velit ultrices, id vulputate eros pellentesque. Suspendisse potenti. Nulla facilisi. Integer non pulvinar tortor, id laoreet orci. Aenean malesuada velit in lectus convallis, id varius nulla eleifend. Donec ac lectus vitae libero aliquet dapibus. Curabitur tempus sollicitudin risus nec venenatis. Cras hendrerit metus id nisl faucibus, non dictum turpis laoreet. Quisque convallis, erat vel scelerisque viverra, mi eros vehicula libero, id varius eros velit ut nisl. Morbi tempus massa at lacus pharetra scelerisque. Ut sed semper ipsum, in viverra ligula. Donec facilisis orci non diam fringilla, nec luctus elit ullamcorper. Mauris nec magna vitae orci malesuada tristique. Nam sit amet orci lacus. Nam et turpis eu orci tristique viverra. Sed sit amet ipsum magna.
         </Typography>
-        <Box display="flex" gap={2} style={{  }}>
+        <Box display="flex" gap={2}>
           <Button 
             variant="contained"
             onClick={handleSnackbarOpen}
             style={{
-              backgroundColor: '#FF00FC',
-              color: '#E1EBFA',
-              fontSize: '20px',
+              backgroundColor: '#00CFFF',
+              color: '#FFFFFF',
+              fontSize: '1rem',
               fontWeight: 700,
-              lineHeight: '26px',
-              width: '100%', 
-              height: '60px',
-              marginRight: '20px',
-              outline: 'none',
+              width: '120px',
+              height: '40px',
+              textTransform: 'none',
             }}
           >
-            Discover CyberVerse
-          </Button>
-          <Button 
-            onClick={() => window.open('https://skyharbor.io', '_blank', 'noopener,noreferrer')}
-            variant="outlined"
-            style={{
-              borderColor: '#FF00FC',
-              color: '#FF00FC',
-              fontSize: '20px',
-              fontFamily: 'pixelFont',
-              fontWeight: 700,
-              lineHeight: '26px',
-              width: '100%', 
-              height: '60px',
-              outline: 'none',
-            }}
-          >
-            Marketplace
+            Play Now
           </Button>
         </Box>
       </Box>
       <Box 
         flex={1} 
         p={2} 
-        style={{
-          display: 'block',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50%', 
-          
-          width: '50%', 
-          overflow: 'hidden', 
-        }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
       >
         <Box
           style={{
             position: 'relative',
-           // 1:1 aspect ratio
-            width: '60%',
-            height: '100%',
-            marginLeft:'15%',
+            paddingTop: '56.25%', // 16:9 aspect ratio
+            width: '100%',
             borderRadius: '8px',
             overflow: 'hidden',
           }}
@@ -116,9 +85,10 @@ const SectionText = () => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             style={{
-              position: 'relative',
-           marginTop:'70px',
-              width: '80%',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
               height: '100%',
               border: 'none',
             }}
