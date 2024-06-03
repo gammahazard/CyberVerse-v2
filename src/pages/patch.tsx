a18dafb53be06bff2a58e3bce103f817484a5aeb
gammahazard <dhaliswag777@gmail.com>
2024-06-03 17:18:55 -0400

    fix patch page styling

--- src/pages/patch.tsx
+++ src/pages/patch.tsx
@@ -1,24 +1,23 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const PatchPage = () => {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+    <Box>
       <Navbar />
-
-      <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        <Box sx={{ flexGrow: 1, width: '100%', display: 'flex', justifyContent: 'center' }}>
-          <iframe 
-            src="https://www.cyberversewiki.com/updates.html" 
-            title="CyberVerse Updates"
-            width="100%"
-            height="100%"
-            style={{ border: 'none' }}
-          ></iframe>
-        </Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <iframe 
+          src="https://www.cyberversewiki.com/updates.html" 
+          title="CyberVerse Updates"
+          style={{ width: '100%', height: '100%', border: 'none' }}
+        ></iframe>
       </Box>
-
       <Footer />
     </Box>
   );
